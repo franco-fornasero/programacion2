0d8eff4631bc4ef451022507dd24405514a0eee3
francofornasero <franco.fornasero@gmail.com>
2020-05-06 16:50:10 -0300

    Ejercicio 7

---
+++ prog2-2020-practica-eventos-master/ResolucionEjercicio/Clases.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResolucionEjercicio
+{
+    public abstract class Producto
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string NumeroSerie { get; set; }
+        public string Identificador
+        {
+            get
+            {
+                return $"{Marca}-{Modelo}-{NumeroSerie}";
+            }
+        }
+
+        public abstract string ObtenerDescripcion();
+    }
+
+    public sealed class Monitor : Producto
+    {
+        public int AñoFabricacion { get; set; }
+        public bool EsProductoNuevo
+        {
+            get
+            {
+                return AñoFabricacion == DateTime.Today.Year;
+            }
+        }
+        public int? Pulgadas { get; set; }
+
+        public override string ObtenerDescripcion()
+        {
+            return $"MONITOR {Marca}-{Modelo} {(Pulgadas.HasValue ? Pulgadas.Value.ToString() : string.Empty)}";
+        }
+    }
+
+    public enum RAM
+    {
+        Dos = 2, Cuatro = 4, Ocho = 8, Dieciseis = 16
+    }
+
+    public sealed class Computadora : Producto
+    {
+        public string Procesador { get; set; }
+        public RAM RAM { get; set; }
+        public string Fabricante { get; set; }
+
+        public override string ObtenerDescripcion()
+        {
+            return $"COMPUTADORA {Marca}-{Modelo} {(int)RAM}GB RAM - {Fabricante}";
+        }
+    }
+
+    public sealed class Teclado : Producto
+    {
+        public bool EsMecanico { get; set; }
+
+        public override string ObtenerDescripcion()
+        {
+            return $"TECLADO {Marca} {Modelo} - {EsMecanico}";
+        }
+    }
+
+    /// <summary>
+    /// Clase Deposito definida como Singleton
+    /// </summary>
+    public class Deposito
+    {
+        private static Deposito instance;
+
+        private Deposito()
+        {
+
+        }
+
+        public static Deposito Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Deposito();
+
+                return instance;
+            }
+        }
+
+        public static List<Monitor> Monitores = new List<Monitor>();
+        public static List<Computadora> Computadoras = new List<Computadora>();
+        public static List<Teclado> Teclados = new List<Teclado>();
+
+        public delegate void ProductoAgregadoEliminadoHandler(ProductoAgregadoEliminadoArgs contexto);
+        public event ProductoAgregadoEliminadoHandler ProductoAgregadoEliminado;
+
+        public void AgregarProducto(Producto producto)
+        {
+            AgregarProductoInterno(producto, this.ObtenerProductos());
+        }
+        
+        private void AgregarProductoInterno(Producto producto, List<Producto> lista)
+        {
+            lista.Add(producto);
+            ProductoAgregadoEliminado(new ProductoAgregadoEliminadoArgs(producto));
+        }
+
+        public void EliminarProducto(string identificador)
+        {            
+            Eliminar(identificador, this.ObtenerProductos());            
+        }
+
+        private void Eliminar(string identificador, List<Producto> lista)
+        {
+            var producto = lista.FirstOrDefault(x => x.Identificador == identificador);
+            if (producto != null)
+            {
+                lista.Remove(producto);
+                ProductoAgregadoEliminado(new ProductoAgregadoEliminadoArgs(producto));
+            }
+        }        
+
+        public List<Producto> ObtenerProductos()
+        {
+            List<Producto> productos = new List<Producto>();
+            
+            productos.AddRange(Deposito.Monitores);
+            productos.AddRange(Deposito.Computadoras);
+            productos.AddRange(Deposito.Teclados);
+
+            return productos.OrderByDescending(x => x is Computadora).ToList();            
+        }
+    }    
+
+    public class ProductoAgregadoEliminadoArgs : EventArgs
+    {
+        //TODO > Que hay de raro aca? esto escala a futuro?
+        public ProductoAgregadoEliminadoArgs(Producto producto)
+        {
+            Producto = producto;
+        }
+        
+        public Producto Producto { get; set; }
+        public string Tipo { get; set; }        
+    }
+}
---
+++ prog2-2020-practica-eventos-master/ResolucionEjercicio/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ResolucionEjercicio
+{
+    static class Validadores
+    {
+        static bool EsRAMCompatible_Op1(this int nro)
+        {
+            try
+            {
+                RAM ram = (RAM)nro;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        static bool EsRAMCompatible_Op2(this string valor)
+        {
+            return Enum.IsDefined(typeof(RAM), valor);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deposito.Instance.ProductoAgregadoEliminado += Instance_ProductoAgregadoEliminado;
+
+            Deposito.Instance.AgregarProducto(new Teclado() { Marca = "T1", Modelo = "Mod T1", NumeroSerie = "S001" });
+            Console.ReadLine();
+
+            Deposito.Instance.AgregarProducto(new Monitor() { Marca = "M1", Modelo = "Mod1", NumeroSerie = "001002", AñoFabricacion = 2019 });
+            Console.ReadLine();
+            Deposito.Instance.AgregarProducto(new Computadora() { Marca = "C2", Modelo = "Mod5", NumeroSerie = "009003", RAM = RAM.Cuatro, Fabricante = "F1", Procesador = "P1" });
+            Console.ReadLine();
+            Deposito.Instance.AgregarProducto(new Monitor() { Marca = "M2", Modelo = "Mod2", NumeroSerie = "001003", AñoFabricacion = 2020 });
+            Console.ReadLine();
+            Deposito.Instance.AgregarProducto(new Computadora() { Marca = "C1", Modelo = "Mod4", NumeroSerie = "009002", RAM = RAM.Cuatro, Fabricante = "F1", Procesador = "P1" });
+            Console.ReadLine();
+            Deposito.Instance.EliminarProducto("M1-Mod1-001002");
+            Console.ReadLine();
+            Deposito.Instance.AgregarProducto(new Monitor() { Marca = "M1", Modelo = "Mod3", NumeroSerie = "001004", AñoFabricacion = 2019, Pulgadas = 32 });
+            Console.ReadLine();
+            Deposito.Instance.AgregarProducto(new Monitor() { Marca = "M2", Modelo = "Mod1", NumeroSerie = "001005", AñoFabricacion = 2020, Pulgadas = 55 });
+            Console.ReadLine();
+            Deposito.Instance.EliminarProducto("C2-Mod5-009003");
+
+            Console.WriteLine("Finalizado...");
+            Console.ReadLine();
+        }
+
+        private static void Instance_ProductoAgregadoEliminado(ProductoAgregadoEliminadoArgs contexto)
+        {
+            Console.Clear();
+            foreach (var item in Deposito.Instance.ObtenerProductos())
+            {
+                if (item.Identificador == contexto.Producto.Identificador)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+
+                Console.WriteLine(item.ObtenerDescripcion());
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+
+        public void BuscarClientes(string nombre, string apellido, int dni)
+        {
+
+        }
+
+        public void BuscarClientes(FiltroCliente filtro)
+        {
+
+        }        
+    }
+
+    class FiltroCliente
+    {
+        public int Dni { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
